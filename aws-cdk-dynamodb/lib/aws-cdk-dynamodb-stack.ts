fb567c335ec3af9919956ac49adc88f8c536362d
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-04-04 12:54:25 -0300

    Update aws-cdk-dynamodb-stack.ts

--- aws-cdk-dynamodb/lib/aws-cdk-dynamodb-stack.ts
+++ aws-cdk-dynamodb/lib/aws-cdk-dynamodb-stack.ts
@@ -2,7 +2,6 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
-
 export class AwsCdkDynamodbStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
